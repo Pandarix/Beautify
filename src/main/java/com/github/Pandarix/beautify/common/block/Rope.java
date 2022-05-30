950f9c75fe7a29e2411ee0cfbcc71aa468bab9ef
Ruven <ruven@uni-bremen.de>
2022-05-30 10:49:23 +0200

    made rope climbable

--- src/main/java/com/github/Pandarix/beautify/common/block/Rope.java
+++ src/main/java/com/github/Pandarix/beautify/common/block/Rope.java
@@ -2,16 +2,21 @@ package com.github.Pandarix.beautify.common.block;
 
 import java.util.List;
 
+import com.github.Pandarix.beautify.core.init.BlockInit;
 import com.github.Pandarix.beautify.util.KeyBoardHelper;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.ChainBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class Rope extends ChainBlock {
 	public Rope(BlockBehaviour.Properties properties) {
@@ -28,9 +33,28 @@ public class Rope extends ChainBlock {
 		}
 
 		if (KeyBoardHelper.isHoldingShift()) {
-			tooltip.add(
-					new TextComponent("\u00A77Acts like Chains. Nice addition to\u00A77 \u00A7ohanging pots.\u00A7o"));
+			tooltip.add(new TextComponent(
+					"\u00A77Placable like chains and climbable. Nice addition to\u00A77 \u00A7ohanging pots.\u00A7o"));
 		}
 		super.appendHoverText(stack, getter, tooltip, flag);
+	}
+
+	@Override
+	public boolean isLadder(BlockState state, LevelReader level, BlockPos pos, LivingEntity entity) {
+		if (state.is(BlockInit.ROPE.get())) {
+			return true;
+		} else {
+			return super.isLadder(state, level, pos, entity);
+		}
+	}
+
+	@Override
+	public boolean makesOpenTrapdoorAboveClimbable(BlockState state, LevelReader level, BlockPos pos,
+			BlockState trapdoorState) {
+		if (state.is(BlockInit.ROPE.get())) {
+			return true;
+		} else {
+			return super.makesOpenTrapdoorAboveClimbable(state, level, pos, trapdoorState);
+		}
 	}
 }
