ee8fe914a6b33b82838f00d0528630279b038185
Ruven <ruven@uni-bremen.de>
2022-08-20 19:11:32 +0200

    fixed loottables

--- src/main/java/com/github/Pandarix/beautify/common/block/HangingPot.java
+++ src/main/java/com/github/Pandarix/beautify/common/block/HangingPot.java
@@ -2,17 +2,18 @@ package com.github.Pandarix.beautify.common.block;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
 import com.github.Pandarix.beautify.util.KeyBoardHelper;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -24,25 +25,29 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LanternBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.common.ToolAction;
 
 public class HangingPot extends LanternBlock {
 	private static final List<Item> validFlowers = Arrays.asList(Items.AIR, Items.ROSE_BUSH, Items.LILAC,
 			Items.BLUE_ORCHID, Items.VINE, Items.SUNFLOWER, Items.PEONY, Items.AZURE_BLUET, Items.RED_TULIP,
 			Items.ORANGE_TULIP, Items.WHITE_TULIP, Items.PINK_TULIP, Items.ALLIUM, Items.DANDELION, Items.POPPY,
 			Items.GLOW_LICHEN, Items.OXEYE_DAISY, Items.LILY_OF_THE_VALLEY, Items.CORNFLOWER, Items.WEEPING_VINES,
-			Items.TWISTING_VINES, Items.WITHER_ROSE, Items.GLOW_BERRIES, Items.SWEET_BERRIES, Items.GRASS);
+			Items.TWISTING_VINES, Items.WITHER_ROSE, Items.GLOW_BERRIES, Items.SWEET_BERRIES, Items.GRASS, Items.FERN);
 
 	// POTFLOWER indicates which index of the flowers List below is active
 	public static final IntegerProperty POTFLOWER = IntegerProperty.create("potflower", 0, validFlowers.size() - 1);
 
+	public static final BooleanProperty GROWN = BooleanProperty.create("grown");
+
 	public HangingPot(Properties properties) {
 		super(properties);
-		this.registerDefaultState(this.defaultBlockState().setValue(POTFLOWER, 0));
+		this.registerDefaultState(this.defaultBlockState().setValue(POTFLOWER, 0).setValue(GROWN, false));
 	}
 
 	// hitbox for the HangingPot
@@ -58,45 +63,67 @@ public class HangingPot extends LanternBlock {
 		return STANDING_SHAPE;
 	}
 
+	@SuppressWarnings("deprecation")
+	@Override
+	public void neighborChanged(BlockState state, Level level, BlockPos pos, Block block,
+			BlockPos blockPos, boolean bool) {
+		if (state.getValue(GROWN)) { //if the plant is grown long
+			if (blockPos.equals(pos.below())
+					&& level.getBlockState(pos.below()).isFaceSturdy(level, pos.below(), Direction.UP)) { //if the neighbour is a model that clips into the pot
+				level.setBlock(pos, state.setValue(GROWN, false), 3);
+				ItemEntity Item = new ItemEntity(level, pos.getX(), pos.getY(), pos.getZ(),
+						new ItemStack(validFlowers.get(state.getValue(POTFLOWER))));
+				level.addFreshEntity(Item);
+				level.playSound(null, pos, SoundEvents.HANGING_ROOTS_BREAK, SoundSource.BLOCKS, 1, 1);
+			}
+		}
+		super.neighborChanged(state, level, pos, block, blockPos, bool);
+	}
+
 	public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand,
 			BlockHitResult pResult) {
 
 		if (!pLevel.isClientSide() && pHand == InteractionHand.MAIN_HAND) {
+			ItemStack playerStack = pPlayer.getItemInHand(pHand); // saving ItemStack
 
-			ItemStack playerStack = pPlayer.getItemInHand(pHand);
-			if (playerStack.getItem().equals(Items.BONE_MEAL) && pState.getValue(POTFLOWER) != 0) {
-				Random rand = new Random();
-				int randomNum = rand.nextInt(3);
-
-				if (randomNum == 0) {
-					pLevel.playSound(null, pPos, SoundEvents.NETHER_SPROUTS_BREAK, SoundSource.BLOCKS, 1, 1);
-					pPlayer.drop(new ItemStack(validFlowers.get(pState.getValue(POTFLOWER))), false);
-					if (!pPlayer.isCreative()) {
-						playerStack.shrink(1);
-					}
-					return InteractionResult.SUCCESS;
-				} else {
+			// growing plant
+			// block below must not be sturdy to prevent clipping models
+			if (playerStack.getItem().equals(Items.BONE_MEAL) && pState.getValue(POTFLOWER) != 0
+					&& !pLevel.getBlockState(pPos.below()).isFaceSturdy(pLevel, pPos.below(), Direction.UP)) {
+				if (!pState.getValue(GROWN)) {
 					pLevel.playSound(null, pPos, SoundEvents.BONE_MEAL_USE, SoundSource.BLOCKS, 1, 1);
 					if (!pPlayer.isCreative()) {
 						playerStack.shrink(1);
 					}
+					pLevel.setBlock(pPos, pState.setValue(GROWN, true), 3);
 					return InteractionResult.SUCCESS;
 				}
 			}
 
+			// when plant is grown -> using shears cuts it down
+			if (playerStack.canPerformAction(ToolAction.get("shears_harvest")) && pState.getValue(POTFLOWER) != 0
+					&& pState.getValue(GROWN)) {
+				pLevel.playSound(null, pPos, SoundEvents.SHEEP_SHEAR, SoundSource.BLOCKS, 1, 1);
+				pLevel.setBlock(pPos, pState.setValue(GROWN, false), 3);
+				ItemEntity Item = new ItemEntity(pLevel, pPos.getX(), pPos.getY(), pPos.getZ(),
+						new ItemStack(validFlowers.get(pState.getValue(POTFLOWER))));
+				pLevel.addFreshEntity(Item);
+				return InteractionResult.SUCCESS;
+			}
+
 			// if there is a flower
 			if (pState.getValue(POTFLOWER) != 0) {
-
 				// giving flower and clearing pot if hand empty
 				if (playerStack.isEmpty()) {
-					pPlayer.setItemInHand(pHand, new ItemStack(validFlowers.get(pState.getValue(POTFLOWER))));
-					pLevel.setBlock(pPos, pState.setValue(POTFLOWER, 0), 3);
+					pPlayer.setItemInHand(pHand, new ItemStack(validFlowers.get(pState.getValue(POTFLOWER)),
+							pState.getValue(GROWN) ? 2 : 1)); // giving 1 or 2 of plant (grown or not)
+					pLevel.setBlock(pPos, pState.setValue(POTFLOWER, 0).setValue(GROWN, false), 3); // emptying the pot
 					pLevel.playSound(null, pPos, SoundEvents.COMPOSTER_READY, SoundSource.BLOCKS, 1, 1);
 					return InteractionResult.SUCCESS;
 				} else if (playerStack.is(validFlowers.get(pState.getValue(POTFLOWER)))
 						&& playerStack.getCount() < playerStack.getMaxStackSize()) {
-					playerStack.grow(1);
-					pLevel.setBlock(pPos, pState.setValue(POTFLOWER, 0), 3);
+					playerStack.grow(pState.getValue(GROWN) ? 2 : 1); // giving 1 or 2 of plant (grown or not)
+					pLevel.setBlock(pPos, pState.setValue(POTFLOWER, 0).setValue(GROWN, false), 3);
 					pLevel.playSound(null, pPos, SoundEvents.AZALEA_LEAVES_BREAK, SoundSource.BLOCKS, 1, 1);
 					return InteractionResult.SUCCESS;
 				}
@@ -130,7 +157,7 @@ public class HangingPot extends LanternBlock {
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
 		super.createBlockStateDefinition(pBuilder);
-		pBuilder.add(POTFLOWER);
+		pBuilder.add(POTFLOWER, GROWN);
 	}
 
 	@Override
@@ -144,7 +171,8 @@ public class HangingPot extends LanternBlock {
 			component.add(Component.literal("Can be placed hanging on blocks, Ropes or on ground as usual.")
 					.withStyle(ChatFormatting.GRAY));
 			component.add(Component.literal("Right click with plants to pot them.").withStyle(ChatFormatting.GRAY));
-			component.add(Component.literal("Right click with Bone Meal to duplicate plant with 1/3 chance.")
+			component.add(Component.literal(
+					"Right click with Bone Meal to grow the plant further. Trim with shears to shorten and recieve plant item.")
 					.withStyle(ChatFormatting.GRAY));
 		}
 
@@ -152,7 +180,7 @@ public class HangingPot extends LanternBlock {
 			component.add(Component.literal("Pottable plants:").withStyle(ChatFormatting.UNDERLINE)
 					.withStyle(ChatFormatting.BOLD).withStyle(ChatFormatting.GRAY));
 			component.add(Component.literal(
-					"All normal flowers + Rose Bushes, Lilacs, Peonies, Sunflowers, Vines, Weeping Vines, Twisting Vines, Glow Lichen, Glow Berries, Sweet Berries, Grass")
+					"All normal flowers + Rose Bushes, Lilacs, Peonies, Sunflowers, Vines, Weeping Vines, Twisting Vines, Glow Lichen, Glow Berries, Sweet Berries, Grass, Fern")
 					.withStyle(ChatFormatting.GRAY));
 		}
 		super.appendHoverText(stack, getter, component, flag);
